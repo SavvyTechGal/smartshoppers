38ec90f599ee5ceac6d606dab60c774018a5883f
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-09 12:22:50 -0500

    added brand component page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrandComponent } from './brand/brand.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,
