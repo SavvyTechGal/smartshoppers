a76b348fdccb30fca81b952ea87b7079850a02e2
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-09 12:25:54 -0500

    added game component page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrandComponent } from './brand/brand.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BrandComponent
+    BrandComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
