75845d4d106055dbe4b4a460cd4b25ecd38645e7
Arifa-B <44043949+Arifa-B@users.noreply.github.com>
2022-03-21 23:49:34 -0400

    added user profile component and service to read json data

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { ServicesComponent } from './services/services.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
@@ -31,7 +32,8 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'services', component: ServicesComponent}
+  { path: 'services', component: ServicesComponent},
+  { path: 'profile', component: ProfileComponent}
 
 ];
 
