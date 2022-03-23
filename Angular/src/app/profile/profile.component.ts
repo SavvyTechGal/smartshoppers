06772365722e74aa4d67a1cf17b36667a247215f
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-03-23 10:21:58 -0400

    show profile info from auth0 token, +profile button to navbar, protected profile route

--- Angular/src/app/profile/profile.component.ts
+++ Angular/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { Chart } from 'chart.js';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({ //need to actually display the users
   selector: 'app-profile',
@@ -9,11 +10,19 @@ import { Chart } from 'chart.js';
 })
 export class ProfileComponent implements OnInit {
 
+  profileJson: string = '';
+
   public users : any = []; //had to add any type
 
-  constructor(private _profileService: ProfileService) { }
+  constructor(
+    private _profileService: ProfileService, 
+    public auth: AuthService) { }
 
   ngOnInit(): void { //remove void
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile))
+    )
+
     this._profileService.getUsers() //returns an observable 
         .subscribe(data => this.users = data); //assign the data that arrives to users array
   }
