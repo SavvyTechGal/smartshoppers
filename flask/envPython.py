0b33509c927c87f8bc5a7ab3d5e79c011c9f75dd
Savana Hughes <savanahughes@gmail.com>
2022-03-29 21:00:31 -0400

    python RDS test

---
+++ flask/envPython.py
@@ -0,0 +1,13 @@
+import os
+
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'postgres',
+            'NAME': os.environ['smart-shoppers'],
+            'USER': os.environ['smartshoppers'],
+            'PASSWORD': os.environ['Smartsho33ers2022'],
+            'HOST': os.environ['smart-shoppers.cqq5zn6lxhjd.us-east-1.rds.amazonaws.com'],
+            'PORT': os.environ['5432'],
+        }
+    }
