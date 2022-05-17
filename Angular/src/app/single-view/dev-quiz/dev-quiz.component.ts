57a2c1a8c00d654db20872eb53365862a54cc90e
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-05-17 15:02:38 -0400

    edited dev quiz

--- Angular/src/app/single-view/dev-quiz/dev-quiz.component.ts
+++ Angular/src/app/single-view/dev-quiz/dev-quiz.component.ts
@@ -12,25 +12,52 @@ import { UserService } from 'src/app/user.service';
 })
 export class DevQuizComponent implements OnInit {
   userEmail: string = '';
-  severalApps: string = ''; //value changed by button click functions
-  appsRank: string = '';
+
+  public submitted: boolean = false;
+
+  //keys used for accessing form values
+  public OSKeys:any[] = ['Windows', 'Mac', 'Chrome'];  
+  public brandKeys:any[] = ['Acer','Apple','ASUS', 'Dell','HP','Lenovo','Microsoft'];
+
+  public AnswersObject: any = {  //will hold edited responses onSubmit
+    role:'',
+    budget: '',
+    os:[],
+    osRank: '',
+    brands:[],
+    brandRank:'',
+    severalApps:'no',
+    severalAppsRank:'',
+    travel:'no',
+    travelRank:'',
+    storage: 'no',
+    storageRank: '',
+    charges: 'no',
+    chargesRank: '',
+    speed: 'no',
+    speedRank: '',
+    heated:'no',
+    heatedRank:''
+  }
+  // severalApps: string = ''; //value changed by button click functions
+  // appsRank: string = '';
   
-  traveler: string = '';
-  travelerRank: string = '';
+  // traveler: string = '';
+  // travelerRank: string = '';
   
-  storage: string = '';
-  storageRank: string = '';
+  // storage: string = '';
+  // storageRank: string = '';
 
-  manyCharges: string = '';
-  chargesRank: string = '';
+  // manyCharges: string = '';
+  // chargesRank: string = '';
 
-  speed: string = '';
-  speedRank: string = '';
+  // speed: string = '';
+  // speedRank: string = '';
 
-  heated: string = '';
-  heatedRank: string = '';
+  // heated: string = '';
+  // heatedRank: string = '';
 
-  brandsArray: string[] = [];
+  // brandsArray: string[] = [];
 
   newUser: boolean = false;  //if user is registered in db
   public currentRole: any;
@@ -41,88 +68,146 @@ export class DevQuizComponent implements OnInit {
   //bools to check which quiz version to display
   casualUser = false;
 
-  yesApps(){
-    this.severalApps = 'Yes';
-  }
-  noApps(){
-    this.severalApps = 'No';
-  }
+  // yesApps(){
+  //   this.severalApps = 'Yes';
+  // }
+  // noApps(){
+  //   this.severalApps = 'No';
+  // }
 
-  yesTravel(){
-    this.traveler = 'Yes';
-  }
-  noTravel(){
-    this.traveler = 'No';
-  }
+  // yesTravel(){
+  //   this.traveler = 'Yes';
+  // }
+  // noTravel(){
+  //   this.traveler = 'No';
+  // }
 
-  yesStorage(){
-    this.storage = 'Yes';
-  }
-  noStorage(){
-    this.storage = 'No';
-  }
+  // yesStorage(){
+  //   this.storage = 'Yes';
+  // }
+  // noStorage(){
+  //   this.storage = 'No';
+  // }
   
-  yesCharges(){
-    this.manyCharges = 'Yes';
-  }
-  noCharges(){
-    this.manyCharges = 'No';
-  }
+  // yesCharges(){
+  //   this.manyCharges = 'Yes';
+  // }
+  // noCharges(){
+  //   this.manyCharges = 'No';
+  // }
 
-  yesSpeed(){
-    this.speed = 'Yes';
-  }
-  noSpeed(){
-    this.speed = 'No';
-  }
+  // yesSpeed(){
+  //   this.speed = 'Yes';
+  // }
+  // noSpeed(){
+  //   this.speed = 'No';
+  // }
 
-  yesHeated(){
-    this.heated = 'Yes';
-  }
-  noHeated(){
-    this.heated = 'No';
-  }
+  // yesHeated(){
+  //   this.heated = 'Yes';
+  // }
+  // noHeated(){
+  //   this.heated = 'No';
+  // }
 
   onSubmit(quizanswers: { value: any; }) //takes in all answers (just checkboxes) from the questionnaire
   {
     console.log('User email: ', this.userEmail);
-
-    console.log('question 1:');
-    console.log(quizanswers.value.appsRank);
-    console.log(this.severalApps);
-
-    console.log('question 2:');
-    console.log(quizanswers.value.travelerRank);
-    console.log(this.traveler);
-
-    console.log('question 3 and 4:');
-    for (let key in quizanswers.value) //looping over form object
-    {
-      if((quizanswers.value)[key] === true)
-      {
-        console.log(key);
-        this.brandsArray.push(key); //add os AND brand choices to brandsArray (CAN FIX IF THIS IS A PROBLEM)
+    for(const brand in this.brandKeys) {
+      let key = this.brandKeys[brand];
+      if((quizanswers.value)[key]) {
+        this.AnswersObject.brands.push(key);
       }
     }
-    console.log(this.brandsArray);
-    console.log(quizanswers.value.osRank);
-    console.log(quizanswers.value.brandsRank);
+
+    this.AnswersObject.brandRank = (quizanswers.value.brandRank).toString();
+
+    for(const OS in this.OSKeys) {
+      let key = this.OSKeys[OS];
+      if((quizanswers.value)[key]) {
+        this.AnswersObject.os.push(key);
+      }
+    }
+
+    this.AnswersObject.osRank = (quizanswers.value.osRank).toString();
+
+    if(quizanswers.value.severalApps) {
+      this.AnswersObject.severalApps = 'yes';
+    }
+
+    this.AnswersObject.severalAppsRank = (quizanswers.value.severalAppsRank).toString();
     
-    console.log('question 5:');
-    console.log(quizanswers.value.storageRank);
-    console.log(this.storage);
+    if(quizanswers.value.travel) {
+      this.AnswersObject.travel = 'yes';
+    }
 
-    console.log('question 6:');
-    console.log(quizanswers.value.chargesRank);
-    console.log(this.manyCharges);
+    this.AnswersObject.travelRank = (quizanswers.value.travelRank).toString();
+    
+    if(quizanswers.value.storage) {
+      this.AnswersObject.storage = 'yes';
+    }
 
-    console.log('question 7:');
-    console.log(quizanswers.value.speedRank);
-    console.log(this.speed);
+    this.AnswersObject.storageRank = (quizanswers.value.storageRank).toString();
+   
+    if(quizanswers.value.charges) {
+      this.AnswersObject.charges = 'yes';
+    }
 
-    console.log('question 8:');
-    console.log(quizanswers.value.heatedRank);
-    console.log(this.heated);
+    this.AnswersObject.chargesRank = (quizanswers.value.chargesRank).toString();
+   
+    if(quizanswers.value.speed) {
+      this.AnswersObject.speed = 'yes';
+    }
+
+    this.AnswersObject.speedRank = (quizanswers.value.speedRank).toString();
+    
+    if(quizanswers.value.heated) {
+      this.AnswersObject.heated = 'yes';
+    }
+
+    this.AnswersObject.heatedRank = (quizanswers.value.heatedRank).toString();
+    
+    this.submitted = true;
+    console.log(this.AnswersObject); //print out responses
+
+    // console.log('question 1:');
+    // console.log(quizanswers.value.appsRank);
+    // console.log(this.severalApps);
+
+    // console.log('question 2:');
+    // console.log(quizanswers.value.travelerRank);
+    // console.log(this.traveler);
+
+    // console.log('question 3 and 4:');
+    // for (let key in quizanswers.value) //looping over form object
+    // {
+    //   if((quizanswers.value)[key] === true)
+    //   {
+    //     console.log(key);
+    //     this.brandsArray.push(key); //add os AND brand choices to brandsArray (CAN FIX IF THIS IS A PROBLEM)
+    //   }
+    // }
+    // console.log(this.brandsArray);
+    // console.log(quizanswers.value.osRank);
+    // console.log(quizanswers.value.brandsRank);
+    
+    // console.log('question 5:');
+    // console.log(quizanswers.value.storageRank);
+    // console.log(this.storage);
+
+    // console.log('question 6:');
+    // console.log(quizanswers.value.chargesRank);
+    // console.log(this.manyCharges);
+
+    // console.log('question 7:');
+    // console.log(quizanswers.value.speedRank);
+    // console.log(this.speed);
+
+    // console.log('question 8:');
+    // console.log(quizanswers.value.heatedRank);
+    // console.log(this.heated);
+
+
     // this.answerService.postAnswer(this.userEmail, '1', this.severalApps, this.appsRank);
 
   }
