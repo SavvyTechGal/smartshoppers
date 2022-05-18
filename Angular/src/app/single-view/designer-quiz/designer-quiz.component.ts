96b9507ed60eea02dc4653590bc95b0af114ee50
Arifa-B <44043949+Arifa-B@users.noreply.github.com>
2022-05-18 15:38:16 -0400

    added budget slider

--- Angular/src/app/single-view/designer-quiz/designer-quiz.component.ts
+++ Angular/src/app/single-view/designer-quiz/designer-quiz.component.ts
@@ -127,6 +127,8 @@ export class DesignerQuizComponent implements OnInit {
     }
 
     this.AnswersObject.heatedRank = (quizanswers.value.heatedRank).toString();
+
+    this.AnswersObject.budget = this.answerService.confirmedBudget; //get user's budget from answer service
     
     this.submitted = true;
     console.log(this.AnswersObject); //print out responses
