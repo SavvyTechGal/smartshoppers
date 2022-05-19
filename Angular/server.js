0da3d543308cda83719e0f2d559394b4b8772736
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2022-05-19 00:46:59 -0400

    workinggit add .

--- Angular/server.js
+++ Angular/server.js
@@ -7,11 +7,11 @@ const app = express();
 
 //Set the base path to the angular-test dist folder
 app.use(express.static(path.join(__dirname, 'dist/smart-shoppers')));
-app.use(express.static(path.join(__dirname, 'dist/smart-shoppers/assets')));
+app.use(express.static('/assets'));
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/smart-shoppers'));
+    res.sendFile(path.join(__dirname, 'dist/smart-shoppers/index.html'));
 });
 
 //Starting server on port 8081
