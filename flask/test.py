4af2aea7ef11713122c0bf79573459ec1949eab3
SavvyTechGal <33135612+SavvyTechGal@users.noreply.github.com>
2022-05-18 22:27:01 -0400

    testing stuff

---
+++ flask/test.py
@@ -0,0 +1,8 @@
+import json
+x = {
+        "email": "petersandy@example.com",
+        "first_name": "Peter",
+        "last_name": "Sandy",
+        "role": "Graphic Designer"
+        }
+print(json.dumps(x))
