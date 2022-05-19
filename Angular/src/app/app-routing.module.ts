481fa474aa8f1f530be6e70083ebd52f4ac1b3cb
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-05-18 22:47:35 -0400

    removed unused components

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,32 +3,32 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 
 import { HomeComponent } from './home/home.component';
-import { BrandComponent } from './brand/brand.component';
-import { GameComponent } from './game/game.component';
-import { GameInputComponent } from './game-input/game-input.component';
-import { TravelComponent } from './travel/travel.component';
-import { WorkComponent } from './work/work.component';
-import { CoderComponent } from './coder/coder.component';
-import { GraphicsComponent } from './graphics/graphics.component';
-import { MoviesComponent } from './movies/movies.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+// import { BrandComponent } from './brand/brand.component';
+// import { GameComponent } from './game/game.component';
+// import { GameInputComponent } from './game-input/game-input.component';
+// import { TravelComponent } from './travel/travel.component';
+// import { WorkComponent } from './work/work.component';
+// import { CoderComponent } from './coder/coder.component';
+// import { GraphicsComponent } from './graphics/graphics.component';
+// import { MoviesComponent } from './movies/movies.component';
+// import { LoginComponent } from './login/login.component';
+// import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { ServicesComponent } from './services/services.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { OsComponent } from './os/os.component';
-import { SeveralAppsComponent } from './several-apps/several-apps.component';
-import { BudgetComponent } from './budget/budget.component';
-import { UserRoleComponent } from './user-role/user-role.component';
+// import { OsComponent } from './os/os.component';
+// import { SeveralAppsComponent } from './several-apps/several-apps.component';
+// import { BudgetComponent } from './budget/budget.component';
+// import { UserRoleComponent } from './user-role/user-role.component';
 import { EditQuestionnaireComponent } from './edit-questionnaire/edit-questionnaire.component';
-import { CompletionComponent } from './completion/completion.component';
+//import { CompletionComponent } from './completion/completion.component';
 import { SingleViewComponent } from './single-view/single-view.component';
 import { DevQuizComponent } from './single-view/dev-quiz/dev-quiz.component';
 import { BudgetRangeComponent } from './single-view/budget-range/budget-range.component';
-import { combineLatestInit } from 'rxjs/internal/observable/combineLatest';
+//import { combineLatestInit } from 'rxjs/internal/observable/combineLatest';
 import { GamerQuizComponent } from './single-view/gamer-quiz/gamer-quiz.component';
 import { DesignerQuizComponent } from './single-view/designer-quiz/designer-quiz.component';
 import { StudentQuizComponent } from './single-view/student-quiz/student-quiz.component';
@@ -38,15 +38,16 @@ import { CasualQuizComponent } from './single-view/casual-quiz/casual-quiz.compo
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch:'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'brand', component: BrandComponent },
-  { path: 'game', component: GameComponent },
-  { path: 'game-input', component: GameInputComponent },
-  { path: 'travel', component: TravelComponent },
-  { path: 'work', component: WorkComponent },
-  { path: 'coder', component: CoderComponent },
-  { path: 'graphics', component: GraphicsComponent },
-  { path: 'movies', component: MoviesComponent },
-  { path: 'login', component: LoginComponent },
+
+  // { path: 'brand', component: BrandComponent },
+  // { path: 'game', component: GameComponent },
+  // { path: 'game-input', component: GameInputComponent },
+  // { path: 'travel', component: TravelComponent },
+  // { path: 'work', component: WorkComponent },
+  // { path: 'coder', component: CoderComponent },
+  // { path: 'graphics', component: GraphicsComponent },
+  // { path: 'movies', component: MoviesComponent },
+  // { path: 'login', component: LoginComponent },
   
   { path: 'about', component: AboutComponent },
   { 
@@ -55,12 +56,11 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   { path: 'services', component: ServicesComponent},
-  { path: 'os', component: OsComponent},
-  { path: 'several-apps', component: SeveralAppsComponent},
-  { path: 'budget', component: BudgetComponent},
-  { path: 'user-role', component: UserRoleComponent},
-  { path: 'completion', component: CompletionComponent},
-  { path: 'single-view', component: SingleViewComponent},
+  { 
+    path: 'single-view', 
+    component: SingleViewComponent,
+    canActivate:[AuthGuard]
+  },
   { path: 'dev-quiz', component: DevQuizComponent},
   { path: 'budget-range', component: BudgetRangeComponent},
   { path: 'gamer-quiz', component: GamerQuizComponent},
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
-import { NgChartsModule } from 'ng2-charts';
 import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BrandComponent } from './brand/brand.component';
-import { GameComponent } from './game/game.component';
-import { GameInputComponent } from './game-input/game-input.component';
-import { TravelComponent } from './travel/travel.component';
-import { WorkComponent } from './work/work.component';
-import { CoderComponent } from './coder/coder.component';
-import { GraphicsComponent } from './graphics/graphics.component';
-import { MoviesComponent } from './movies/movies.component';
-import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { ServicesComponent } from './services/services.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { PersonComponent } from './person/person.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ProfileService } from './profile.service';
 import { LoginButtonComponent } from './login-button/login-button.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
@@ -36,16 +20,10 @@ import { SignupButtonComponent } from './signup-button/signup-button.component';
 import { UserService } from './user.service';
 import { ProductService } from './product.service';
 import { AnswersService } from './answers.service';
-import { ProductDisplayComponent } from './product-display/product-display.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { OsComponent } from './os/os.component';
-import { SeveralAppsComponent } from './several-apps/several-apps.component';
-import { BudgetComponent } from './budget/budget.component';
-import { UserRoleComponent } from './user-role/user-role.component';
 import { EditQuestionnaireComponent } from './edit-questionnaire/edit-questionnaire.component';
-import { CompletionComponent } from './completion/completion.component';
 import { SingleViewComponent } from './single-view/single-view.component';
 import { DevQuizComponent } from './single-view/dev-quiz/dev-quiz.component';
 import { BudgetRangeComponent } from './single-view/budget-range/budget-range.component';
@@ -60,36 +38,17 @@ import { CasualQuizComponent } from './single-view/casual-quiz/casual-quiz.compo
   declarations: [
     AppComponent,
     HomeComponent,
-    BrandComponent,
-    GameComponent,
-    GameInputComponent,
-    TravelComponent,
-    WorkComponent,
-    CoderComponent,
-    GraphicsComponent,
-    MoviesComponent,
-    DoughnutChartComponent,
-    LoginComponent,
-    SignupComponent,
     AboutComponent,
     ProductsComponent,
     ServicesComponent,
     SignupFormComponent,
-    LoginFormComponent,
-    PersonComponent,
     ProfileComponent,
     LoginButtonComponent,
     NavbarComponent,
     LogoutButtonComponent,
     SignupButtonComponent,
-    ProductDisplayComponent,
     EditProfileComponent,
-    OsComponent,
-    SeveralAppsComponent,
-    BudgetComponent,
-    UserRoleComponent,
     EditQuestionnaireComponent,
-    CompletionComponent,
     SingleViewComponent,
     DevQuizComponent,
     BudgetRangeComponent,
@@ -102,7 +61,7 @@ import { CasualQuizComponent } from './single-view/casual-quiz/casual-quiz.compo
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgChartsModule,
+   
     RecaptchaModule,
     RecaptchaFormsModule,
     MatPaginatorModule,
@@ -115,7 +74,7 @@ import { CasualQuizComponent } from './single-view/casual-quiz/casual-quiz.compo
     BrowserAnimationsModule,
     
   ],
-  providers: [ProfileService, UserService, ProductService, AnswersService],
+  providers: [UserService, ProductService, AnswersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
